fa63a2a4946cf9b493b1b2ccecc06e84b4198b2e
Mark <mark@gmail.com>
2026-04-14 18:20:05 +0300

    fix port for render

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const http = require("http");
 
+const countries = [
+  { id: 1, name: "Austria", capital: "Vienna", population: 8901064, currency: "Euro", language: "German" },
+  { id: 2, name: "Belgium", capital: "Brussels", population: 11539328, currency: "Euro", language: "Dutch, French, German" },
+  { id: 3, name: "Bulgaria", capital: "Sofia", population: 7000039, currency: "Bulgarian Lev", language: "Bulgarian" },
+  { id: 4, name: "Croatia", capital: "Zagreb", population: 4067500, currency: "Croatian Kuna", language: "Croatian" },
+  { id: 5, name: "Cyprus", capital: "Nicosia", population: 875900, currency: "Euro", language: "Greek, Turkish" },
+  { id: 6, name: "Czech Republic", capital: "Prague", population: 10649800, currency: "Czech Koruna", language: "Czech" },
+  { id: 7, name: "Denmark", capital: "Copenhagen", population: 5792202, currency: "Danish Krone", language: "Danish" },
+  { id: 8, name: "Estonia", capital: "Tallinn", population: 1324820, currency: "Euro", language: "Estonian" }
+];
+
 
 const personInfo = {
     info: {
@@ -31,12 +42,28 @@ const server = http.createServer((req, res) => {
 <p>${hobbyList}</p>
         `);
 
-    } else {
+    } else if (req.url === "/countries") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ countries }));
+}
+    else if (req.url.startsWith("/countries/")) {
 
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Home page");
+    const id = parseInt(req.url.split("/")[2]);
 
+    const country = countries.find(c => c.id === id);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    if (country) {
+       return res.end(JSON.stringify(country));
+     } else {
+       return res.end(JSON.stringify({ message: "Country not found" }));
     }
+}
+else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+}
 
 });
 
